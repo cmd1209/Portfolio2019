e7f3cb0a6b843e33ac4ee52f27f65e6d0bf992d1
chaddanford <chaddanford@Chads-MBP.fritz.box>
2018-11-11 22:41:45 +0100

    sunday night work

--- start.php
+++ start.php
@@ -12,35 +12,31 @@
 <section class="linesgradient vh100 flex flexcolumn flexcenter dbg" id="section1">
 	<?php include('herologo.php'); ?>
 	<div class="container centered bounce-in-right delay3">
-		<h1>
-			<?php echo get_bloginfo( 'description' ); ?>
-		</h1>
+		<h1><em>Creating</em> Making Coding Shooting Framing <em>Editing</em> Developing Brainstorming like it‘s <em>1999</em>.</h1>
 	</div>
-	<div class="downarrow">
-		<span></span>
-	</div>
+	<a href="#section2" class="downarrow"><span></span></a>
 </section>
 
 <section class="vh100 flex flexcolumn flexcenter" id="section2">
 	<div class="container centered hidden" id="section2intro">
-		<?php the_content(); // Dynamic Content ?>
+			<?php the_content(); // Dynamic Content ?>
 	</div>
 </section>
 
-<section class="linesgradient vh150 flex flexcolumn flexcenter dbg" id="section3">
-	<div class="container centered" >
+<section class="vh150 dbg" id="section3">
+
 			
 		<?php // the query
 			$the_query = new WP_Query( 'post_type=post&cat=5'); ?>
 			<?php if ( $the_query->have_posts() ) : ?>
 			<!-- pagination here -->
 			<!-- the loop -->
-			<div class="textwrap hidden" id="projectintro">
+			<!-- <div class="textwrap hidden" id="projectintro">
 				<h2>Recent Projects</h2>
 				<p>I've had the pleasure of working on many wonderful web projects of the past decade or so.</p>
-			</div>
+			</div> -->
 			
-			<div class="grid grid--3l" id="projectgrid">
+			<div class="project-grid project-grid--3l" id="projectgrid">
 				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 					
 					<?php $hero = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' ); ?>
@@ -59,8 +55,5 @@
 			<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 			<?php endif; ?>
 
-
-
-	</div>
 </section>
 <?php get_footer(); ?>
