4ed5fb5bcf64191b682ed5a876d512d1f5e2449a
chaddanford <chaddanford@Chads-MBP.fritz.box>
2018-11-11 14:17:10 +0100

    sunday early afternoon

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,19 @@
 	$(function () {
 		'use strict';
 
-		$('#section2').one('inview', function(event, isInView) {
+		$('#section1').on('inview', function(event, isInView) {
+			if (isInView) {
+			  $('header.startheader').addClass('hidden');
+			  $('header.startheader').removeClass('slide-in-top');
+			} else {
+				// element has gone out of viewport
+				$('header.startheader').addClass('slide-in-top');
+				$('header.startheader').removeClass('hidden');
+			}
+		});
 
+
+		$('#section2').one('inview', function(event, isInView) {
 			if (isInView) {
 			  $('#section2intro').addClass('bounce-in-left');
 			  $('#section2intro').removeClass('hidden');
@@ -32,6 +43,7 @@
 			}
 		});
 
+		
 		$('#projectgrid').on('inview', function(event, isInView) {
 			if (isInView) {
 				$('.project').addClass('slide-in-fwd-center ');
@@ -42,5 +54,12 @@
 			}
 		});
 
+		$(document).on('click', '.activator', function () {
+			$(this).toggleClass('active');
+			$(".startheader-logo").toggleClass('slideoffleft');
+			$(".activator").toggleClass('slideoffleft');
+			$("nav.nav").toggleClass('offleft');
+		});
 	});
+
 })(jQuery, this);
